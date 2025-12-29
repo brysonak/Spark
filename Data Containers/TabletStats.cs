f2bd4a86c7e3184529bf128d601ff9b76fc29b81
he_is_the_cat <125207670+heisthecat31@users.noreply.github.com>
2025-12-29 05:42:43 +0000

    Refactor player stats data extraction logic
    Refactor data extraction for player statistics to improve readability and maintainability.

--- Data Containers/TabletStats.cs
+++ Data Containers/TabletStats.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -48,74 +48,149 @@ namespace Spark
 
 				discord_id = DiscordOAuth.DiscordUserID;
 
-				player_id = long.Parse(((string)data["xplatformid"])?.Split("-").Last() ?? "0");
-				player_name = (string)data["displayname"];
+				var xplatformidToken = data["xplatformid"];
+				if (xplatformidToken != null)
+				{
+					var xplatformid = (string)xplatformidToken;
+					if (!string.IsNullOrEmpty(xplatformid))
+					{
+						var parts = xplatformid.Split("-");
+						if (parts.Length > 0 && long.TryParse(parts.Last(), out long parsedId))
+						{
+							player_id = parsedId;
+						}
+					}
+				}
+				
+				player_name = data["displayname"]?.Value<string>();
+				
 				if (data["social"] != null)
 				{
-					ghosted_count = (int)data["social"]["ghostcount"];
-					muted_count = (int)data["social"]["mutecount"];
+					var social = data["social"];
+					ghosted_count = social["ghostcount"]?.Value<int>() ?? 0;
+					muted_count = social["mutecount"]?.Value<int>() ?? 0;
 				}
 
-				update_time = (int)data["updatetime"];
-				if (data.Contains("creationtime"))
+				update_time = data["updatetime"]?.Value<int>() ?? 0;
+				
+				var creationTimeToken = data["creationtime"] ?? data["createtime"];
+				if (creationTimeToken != null)
 				{
-					creation_time = (int)data["creationtime"];
-				}
-				else if (data.Contains("createtime"))
-				{
-					creation_time = (int)data["createtime"];
+					creation_time = creationTimeToken.Value<int>();
 				}
 
-				if (data["purchasedcombat"] != null)
-					purchased_combat = (int)data["purchasedcombat"];
+				purchased_combat = data["purchasedcombat"]?.Value<int>() ?? 0;
+				
 				if (data["stats"]?["arena"] == null) return;
-				level = (int)data["stats"]["arena"]["Level"]?["val"];
+				
+				var arena = data["stats"]["arena"];
+				var levelToken = arena["Level"];
+				if (levelToken != null)
+				{
+					level = levelToken["val"]?.Value<int>() ?? 0;
+				}
+				
 				if (level > 1)
 				{
-					highest_stuns = (int)(data["stats"]["arena"]["HighestStuns"]?["val"] ?? 0);
-					goal_score_percentage = (float)(data["stats"]["arena"]["GoalScorePercentage"]?["val"] ?? 0);
-					two_point_goals = (int)(data["stats"]["arena"]["TwoPointGoals"]?["val"] ?? 0);
-					highest_saves = (int)(data["stats"]["arena"]["HighestSaves"]?["val"] ?? 0);
-					avg_points_per_game = (float)(data["stats"]["arena"]["AveragePointsPerGame"]?["val"] ?? 0) / (int)(data["stats"]["arena"]["AveragePointsPerGame"]?["cnt"] ?? 0);
-					stuns = (int)(data["stats"]["arena"]["Stuns"]?["val"] ?? 0);
-					stun_percentage = (float)(data["stats"]["arena"]["StunPercentage"]?["val"] ?? 0);
-					arena_wins = (int)(data["stats"]["arena"]["ArenaWins"]?["val"] ?? 0);
-					arena_win_percentage = (float)(data["stats"]["arena"]["ArenaWinPercentage"]?["val"] ?? 0);
-					shots_on_goal_against = (int)(data["stats"]["arena"]["ShotsOnGoalAgainst"]?["val"] ?? 0);
-					shots_on_goal = (int)(data["stats"]["arena"]["ShotsOnGoal"]?["val"] ?? 0);
-					hat_tricks = (int)(data["stats"]["arena"]["HatTricks"]?["val"] ?? 0);
-					highest_points = (int)(data["stats"]["arena"]["HighestPoints"]?["val"] ?? 0);
-					possession_time = (float)(data["stats"]["arena"]["PossessionTime"]?["val"] ?? 0);
-					blocks = (int)(data["stats"]["arena"]["Blocks"]?["val"] ?? 0);
-					bounce_goals = (int)(data["stats"]["arena"]["BounceGoals"]?["val"] ?? 0);
-					stuns_per_game = (float)(data["stats"]["arena"]["StunsPerGame"]?["val"] ?? 0) / (int)(data["stats"]["arena"]["StunsPerGame"]?["cnt"] ?? 0);
-					highest_arena_mvp_streak = (int)(data["stats"]["arena"]["HighestArenaMVPStreak"]?["val"] ?? 0);
-					arena_ties = (int)(data["stats"]["arena"]["ArenaTies"]?["val"] ?? 0);
-					saves_per_game = (float)(data["stats"]["arena"]["SavesPerGame"]?["val"] ?? 0) / (int)(data["stats"]["arena"]["SavesPerGame"]?["cnt"] ?? 0);
-					catches = (int)(data["stats"]["arena"]["Catches"]?["val"] ?? 0);
-					goal_save_percentage = (float)(data["stats"]["arena"]["GoalSavePercentage"]?["val"] ?? 0);
-					goals_per_game = (float)(data["stats"]["arena"]["GoalsPerGame"]?["val"] ?? 0) / (int)(data["stats"]["arena"]["GoalsPerGame"]?["cnt"] ?? 0);
-					current_arena_mvp_streak = (int)(data["stats"]["arena"]["CurrentArenaMVPStreak"]?["val"] ?? 0);
-					jousts_won = (int)(data["stats"]["arena"]["JoustsWon"]?["val"] ?? 0);
-					passes = (int)(data["stats"]["arena"]["Passes"]?["val"] ?? 0);
-					three_point_goals = (int)(data["stats"]["arena"]["ThreePointGoals"]?["val"] ?? 0);
-					assists_per_game = (float)(data["stats"]["arena"]["AssistsPerGame"]?["val"] ?? 0) / (int)(data["stats"]["arena"]["AssistsPerGame"]?["cnt"] ?? 0);
-					current_arena_win_streak = (int)(data["stats"]["arena"]["CurrentArenaWinStreak"]?["val"] ?? 0);
-					assists = (int)(data["stats"]["arena"]["Assists"]?["val"] ?? 0);
-					clears = (int)(data["stats"]["arena"]["Clears"]?["val"] ?? 0);
-					average_top_speed_per_game = (float)(data["stats"]["arena"]["AverageTopSpeedPerGame"]?["val"] ?? 0) / (int)(data["stats"]["arena"]["AverageTopSpeedPerGame"]?["cnt"] ?? 0);
-					average_possession_time_per_game = (float)(data["stats"]["arena"]["AveragePossessionTimePerGame"]?["val"] ?? 0) / (int)(data["stats"]["arena"]["AveragePossessionTimePerGame"]?["cnt"] ?? 0);
-					arena_losses = (int)(data["stats"]["arena"]["ArenaLosses"]?["val"] ?? 0);
-					top_speeds_total = (float)(data["stats"]["arena"]["TopSpeedsTotal"]?["val"] ?? 0);
-					arena_mvps = (int)(data["stats"]["arena"]["ArenaMVPs"]?["val"] ?? 0);
-					arena_mvp_percentage = (float)(data["stats"]["arena"]["ArenaMVPPercentage"]?["val"] ?? 0);
-					punches_recieved = (int)(data["stats"]["arena"]["PunchesReceived"]?["val"] ?? 0);
-					block_percentage = (float)(data["stats"]["arena"]["BlockPercentage"]?["val"] ?? 0);
-					points = (int)(data["stats"]["arena"]["Points"]?["val"] ?? 0);
-					saves = (int)(data["stats"]["arena"]["Saves"]?["val"] ?? 0);
-					interceptions = (int)(data["stats"]["arena"]["Interceptions"]?["val"] ?? 0);
-					goals = (int)(data["stats"]["arena"]["Goals"]?["val"] ?? 0);
-					steals = (int)(data["stats"]["arena"]["Steals"]?["val"] ?? 0);
+					highest_stuns = arena["HighestStuns"]?["val"]?.Value<int>() ?? 0;
+					goal_score_percentage = arena["GoalScorePercentage"]?["val"]?.Value<float>() ?? 0;
+					two_point_goals = arena["TwoPointGoals"]?["val"]?.Value<int>() ?? 0;
+					highest_saves = arena["HighestSaves"]?["val"]?.Value<int>() ?? 0;
+					
+					var avgPointsToken = arena["AveragePointsPerGame"];
+					if (avgPointsToken != null)
+					{
+						float val = avgPointsToken["val"]?.Value<float>() ?? 0;
+						int cnt = avgPointsToken["cnt"]?.Value<int>() ?? 0;
+						avg_points_per_game = cnt > 0 ? val / cnt : 0;
+					}
+					
+					stuns = arena["Stuns"]?["val"]?.Value<int>() ?? 0;
+					stun_percentage = arena["StunPercentage"]?["val"]?.Value<float>() ?? 0;
+					arena_wins = arena["ArenaWins"]?["val"]?.Value<int>() ?? 0;
+					arena_win_percentage = arena["ArenaWinPercentage"]?["val"]?.Value<float>() ?? 0;
+					shots_on_goal_against = arena["ShotsOnGoalAgainst"]?["val"]?.Value<int>() ?? 0;
+					shots_on_goal = arena["ShotsOnGoal"]?["val"]?.Value<int>() ?? 0;
+					hat_tricks = arena["HatTricks"]?["val"]?.Value<int>() ?? 0;
+					highest_points = arena["HighestPoints"]?["val"]?.Value<int>() ?? 0;
+					possession_time = arena["PossessionTime"]?["val"]?.Value<float>() ?? 0;
+					blocks = arena["Blocks"]?["val"]?.Value<int>() ?? 0;
+					bounce_goals = arena["BounceGoals"]?["val"]?.Value<int>() ?? 0;
+					
+					var stunsPerGameToken = arena["StunsPerGame"];
+					if (stunsPerGameToken != null)
+					{
+						float val = stunsPerGameToken["val"]?.Value<float>() ?? 0;
+						int cnt = stunsPerGameToken["cnt"]?.Value<int>() ?? 0;
+						stuns_per_game = cnt > 0 ? val / cnt : 0;
+					}
+					
+					highest_arena_mvp_streak = arena["HighestArenaMVPStreak"]?["val"]?.Value<int>() ?? 0;
+					arena_ties = arena["ArenaTies"]?["val"]?.Value<int>() ?? 0;
+					
+					var savesPerGameToken = arena["SavesPerGame"];
+					if (savesPerGameToken != null)
+					{
+						float val = savesPerGameToken["val"]?.Value<float>() ?? 0;
+						int cnt = savesPerGameToken["cnt"]?.Value<int>() ?? 0;
+						saves_per_game = cnt > 0 ? val / cnt : 0;
+					}
+					
+					catches = arena["Catches"]?["val"]?.Value<int>() ?? 0;
+					goal_save_percentage = arena["GoalSavePercentage"]?["val"]?.Value<float>() ?? 0;
+					
+					var goalsPerGameToken = arena["GoalsPerGame"];
+					if (goalsPerGameToken != null)
+					{
+						float val = goalsPerGameToken["val"]?.Value<float>() ?? 0;
+						int cnt = goalsPerGameToken["cnt"]?.Value<int>() ?? 0;
+						goals_per_game = cnt > 0 ? val / cnt : 0;
+					}
+					
+					current_arena_mvp_streak = arena["CurrentArenaMVPStreak"]?["val"]?.Value<int>() ?? 0;
+					jousts_won = arena["JoustsWon"]?["val"]?.Value<int>() ?? 0;
+					passes = arena["Passes"]?["val"]?.Value<int>() ?? 0;
+					three_point_goals = arena["ThreePointGoals"]?["val"]?.Value<int>() ?? 0;
+					
+					var assistsPerGameToken = arena["AssistsPerGame"];
+					if (assistsPerGameToken != null)
+					{
+						float val = assistsPerGameToken["val"]?.Value<float>() ?? 0;
+						int cnt = assistsPerGameToken["cnt"]?.Value<int>() ?? 0;
+						assists_per_game = cnt > 0 ? val / cnt : 0;
+					}
+					
+					current_arena_win_streak = arena["CurrentArenaWinStreak"]?["val"]?.Value<int>() ?? 0;
+					assists = arena["Assists"]?["val"]?.Value<int>() ?? 0;
+					clears = arena["Clears"]?["val"]?.Value<int>() ?? 0;
+					
+					var avgTopSpeedToken = arena["AverageTopSpeedPerGame"];
+					if (avgTopSpeedToken != null)
+					{
+						float val = avgTopSpeedToken["val"]?.Value<float>() ?? 0;
+						int cnt = avgTopSpeedToken["cnt"]?.Value<int>() ?? 0;
+						average_top_speed_per_game = cnt > 0 ? val / cnt : 0;
+					}
+					
+					var avgPossessionToken = arena["AveragePossessionTimePerGame"];
+					if (avgPossessionToken != null)
+					{
+						float val = avgPossessionToken["val"]?.Value<float>() ?? 0;
+						int cnt = avgPossessionToken["cnt"]?.Value<int>() ?? 0;
+						average_possession_time_per_game = cnt > 0 ? val / cnt : 0;
+					}
+					
+					arena_losses = arena["ArenaLosses"]?["val"]?.Value<int>() ?? 0;
+					top_speeds_total = arena["TopSpeedsTotal"]?["val"]?.Value<float>() ?? 0;
+					arena_mvps = arena["ArenaMVPs"]?["val"]?.Value<int>() ?? 0;
+					arena_mvp_percentage = arena["ArenaMVPPercentage"]?["val"]?.Value<float>() ?? 0;
+					punches_recieved = arena["PunchesReceived"]?["val"]?.Value<int>() ?? 0;
+					block_percentage = arena["BlockPercentage"]?["val"]?.Value<float>() ?? 0;
+					points = arena["Points"]?["val"]?.Value<int>() ?? 0;
+					saves = arena["Saves"]?["val"]?.Value<int>() ?? 0;
+					interceptions = arena["Interceptions"]?["val"]?.Value<int>() ?? 0;
+					goals = arena["Goals"]?["val"]?.Value<int>() ?? 0;
+					steals = arena["Steals"]?["val"]?.Value<int>() ?? 0;
 				}
 			}
 			catch (Exception e)
